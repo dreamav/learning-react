70172e3032db19ab45b5635bd56403c9173418f2
Mevericks <reloved@gmail.com>
2018-10-23 15:58:39 +0300

    императивный способ написать часы (добавил комментарии)

--- ES6-intro/Clock/clock-imperative.js
+++ ES6-intro/Clock/clock-imperative.js
@@ -11,10 +11,11 @@ function logClockTime() {
 }
 
 function getClockTime() {
-
+	// Получение текущего времени
     var date = new Date()
     var time = ""
 
+    // Выстраивание последовательности показания часов
     var time = {
         hours: date.getHours(),
         minutes: date.getMinutes(),
@@ -22,6 +23,7 @@ function getClockTime() {
         ampm: "AM"
     }
 
+    // Преобразование показаний времени в человекопонятный вид
     if (time.hours == 12){
         time.ampm = "PM"
     }else if (time.hours > 12){
@@ -29,18 +31,21 @@ function getClockTime() {
     	time.hours -= 12
     }
 
+    // Добавление 0 к часам чтоб получалось 2 цифры
     if(time.hours < 10){
     	time.hours = "0" + time.hours
     }
-
+	// Добавление 0 к минутам чтоб получалось 2 цифры
     if(time.minutes < 10){
     	time.minutes = "0" + time.minutes
     }
 
+	// Добавление 0 к секундам чтоб получалось 2 цифры
     if(time.seconds < 10){
     	time.seconds = "0" + time.seconds
     }
 
+    // Вывод в формате чч:мм:сс AM/PM
     return time.hours + ":"
     	+ time.minutes + ":"
     	+ time.seconds + " "
