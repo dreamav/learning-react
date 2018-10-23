d257d8af2d542b786ffd5e7374c9229a87465474
Mevericks <reloved@gmail.com>
2018-10-23 13:27:34 +0300

    рекурсия

---
+++ ES6-intro/Functional_conceptions/deepPick.js
@@ -0,0 +1,22 @@
+var dan = {
+    type: "person",
+    data: {
+        gender: "male",
+        info: {
+            id: 22,
+            fullname:{
+                first: "Dan",
+                last: "Deacon"
+            }
+        }
+    }
+}
+
+const deepPick = (fields, object = {}) => {
+    const [first, ...remaining] = fields.split(".")
+    return (remaining.length) ?
+        deepPick(remaining.join("."), object[first]) :
+        object[first]
+}
+
+console.log(deepPick("data.info.fullname.first", dan))
