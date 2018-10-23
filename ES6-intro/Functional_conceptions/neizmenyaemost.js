ab449c1c563712d4bff48ac0ebc8d6c7d54990db
Mevericks <reloved@gmail.com>
2018-10-23 09:47:16 +0300

    неизменяемость

---
+++ ES6-intro/Functional_conceptions/neizmenyaemost.js
@@ -0,0 +1,20 @@
+let list = [
+    {title: "Red"},
+    {title: "lawn"},
+    {title: "Pink"}
+]
+
+// var addColor = function (title,colors) {
+//     colors.push({title:title})
+//     return colors
+// }
+//
+// console.log(addColor("Green",list).length)
+// console.log(list.length)
+//
+//
+// var addColor1 = (title,array) => array.concat({title})
+// console.log(addColor1("Green green",list).length)
+// console.log(list.length)
+
+
