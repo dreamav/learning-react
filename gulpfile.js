5e4694fa859a01a039dfcb271d3f22dac1ae5ee0
Mevericks <reloved@gmail.com>
2018-10-28 12:58:30 +0200

    переместил папки

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 	babel = require('gulp-babel');
 
 gulp.task("transpile", function(){
-	return gulp.src('./ES6-intro/Clock/**/*.js')
+	return gulp.src('./02-javaScript-new-syntax/Clock/**/*.js')
 		.pipe(babel())
-		.pipe(gulp.dest('./ES6-intro/Clock/js/'))
+		.pipe(gulp.dest('./02-javaScript-new-syntax/Clock/js/'))
 })
